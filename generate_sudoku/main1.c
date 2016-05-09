f2d00341c0dfd58f4ed89b12e4aab4b54d264797
fusio-wu <jiafuwu0422@gmail.com>
2016-05-08 21:27:29 -0700

    May 8 9:27pm
    1. Add pre assignment of numbers
    2. Add random number generation

---
+++ generate_sudoku/main1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+ 
+int main()
+{
+//initialize random number generator 
+
+  srand(time(NULL));
+
+FILE *myFile;
+FILE* fp;
+fp = fopen ("generated_sudoku.txt", "w+");
+myFile=fopen("test.txt", "r");
+//read file into array
+int numArray[729];
+int i;
+int n=9;
+for (i=1; i<=n*n*n; i++){
+	fscanf(myFile, "%d", &numArray[i]);
+}
+/*
+for (i=1; i<=n*n*n; i++){
+	printf("Number: %d\n", numArray[i]);
+}
+*/
+int c, r, k;
+
+//double p = (double) rand()/RAND_MAX;
+
+for (c=1; c<=n; c++){
+	for(r=1;r<=n;r++){
+		for (k=1;k<=n; k++){
+			if (numArray[n*(r-1)+n*n*(c-1)+k]>0){
+				if ((double) rand()/RAND_MAX>0.5){
+					fprintf(fp,"%d ", k);
+				}
+				else fprintf(fp, "_ ");
+			}
+		}
+
+	
+	}
+fprintf(fp, "\n");
+
+
+}
+
+fclose(fp);
+return 0;
+}
+
